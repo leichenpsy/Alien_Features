b9ac8dfa5f0916a5e6ddd728670dcd049085b87b
leichenpsy <leichen.psy@gmail.com>
2026-03-31 15:17:50 -0400

    Create color ring snippet.py

---
+++ color ring snippet.py
@@ -0,0 +1,6 @@
+### virtual environment: alien_features
+### installed packages in the virtual environment:
+# - Psychopy v2026.1.2
+# - Python v3.10.12
+
+### This code snippet creates a color ring using Psychopy. The color ring is made up of multiple segments, each with a different color. The colors are generated using the HSV color space, which allows for easy creation of a spectrum of colors.
